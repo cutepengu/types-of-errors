8ef0a29d7a0c1255ac8b9ce54264774db90eceb5
ErinLee <erin.lee22@education.nsw.gov.au>
2025-03-21 15:58:36 +1100

    done

---
+++ 1.py
@@ -0,0 +1,6 @@
+def calculate_area(radius):
+    area = '3.14 x (radius) x 2'
+    return area
+
+print(calculate_area(5))
+print(calculate_area(3))
---
+++ 6.py
@@ -0,0 +1,6 @@
+def calculate_area(diameter):
+    return math.pi * diameter ** 2
+
+print(calculate_area(5))
+
+# math is not defined
